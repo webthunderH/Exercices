2787502e4ee09dd39eab042b8a52bd7b8650dcce
ThunderH <henry.dedonder@gmail.com>
2017-02-08 15:12:43 +0100

    Upgrade Exercices

--- findAWord.cpp
+++ findAWord.cpp
@@ -1,12 +1,30 @@
 #include <string>
 #include <random>
 #include <fstream>
+#include <ctime>
 using namespace std;
 
 string findAWord()
 {
-    ifstream readingStream("SecondProject/dico.txt");
-    string findedWord;
+    string findWord;
+    ifstream readingStream("/Users/henrydedonder/Documents/C++_learning/SecondProject/SecondProject/dico.txt");
+    if(readingStream)
+    {
 
-    return
+      string ligne; //Une variable pour stocker les lignes lues
+      readingStream.seekg(0, ios::end);
+      int maxRange = readingStream.tellg();
+      srand((int)time(0));
+      int random = rand()%maxRange;
+      readingStream.seekg(random,ios::beg);
+      readingStream>>ligne;
+      findWord=ligne;
+      readingStream.close();
+    }
+    else
+    {
+        findWord="Got a problem with reading file";
+    }
+
+    return findWord;
 }
--- main.cpp
+++ main.cpp
@@ -2,62 +2,90 @@
 #include <string>
 #include <math.h>
 #include <cstdlib>
+#include <fstream>
 #include "shakerHeader.h"
+#include "findAWord.h"
 
 using namespace std;
 
 // Game ùystery word on console.
 int main()
 {
-
-    string answerOfPlayer, word; // Will be used to choose if we use the dictionnary or let two players play.
-    cout<<"Choose solo or multiplayer please type (solo or multiplayer)"<<endl;
-
-    while(answerOfPlayer!="solo" || answerOfPlayer !="multiplayer")// Test if the player choose the right type of game
+    bool wantToPlayAgain=true;
+    do
     {
-        cin>>answerOfPlayer;
-        cout<<"Please type solo or multiplayer"<<endl;
-    }
+        string answerOfPlayer, word; // Will be used to choose if we use the dictionnary or let two players play.
+        int numberOfPlayer(0);
+        cout<<"Choose solo or multiplayer please type (solo or multiplayer)"<<endl;
 
-    std::system("clear");// clear the console to have better idea that we play
-    if(answerOfPlayer=="solo")
-    {
-        cout<<"You choose solo play so a word will be choose for you"<<endl;
-
-    }
-    else
-    {
-        cout<<"You choose multiplayer play so choose a word"<< endl;
-        cout<<"Player one add a word"<< endl;
-        cin>> word;
-        std::system("clear");
-    }
-    int compteur(0);// variable to test how many time you faile
-    //1 Variable string to get word
-
-    bool wordFound(false), wordNotFound(false);// variable who say if you win or not
-
-    cout<<"Word is shaked :"<<shaker(word) <<endl;
-    //2 shake letter
-
-    //3 ask until the word is found
-    while(wordFound== false || wordNotFound==false)
-    {
-        string playerTwoWord;
-        cout<<"enter a word"<< endl;
-
-        cin>>playerTwoWord;
-        if(playerTwoWord==word)
+        while(numberOfPlayer==0)// Test if the player choose the right type of game
         {
-         wordFound=true;
-         cout<<"Nice shot you win"<<endl;
+            cin>>answerOfPlayer;
+            if (answerOfPlayer=="solo")
+            {
+                numberOfPlayer=1;
+            }
+            else if (answerOfPlayer=="multiplayer")
+            {
+                numberOfPlayer=2;
+            }
+            cout<<"Please type solo or multiplayer"<<endl;
         }
-        compteur++;
-        if (compteur==5)
+
+        std::system("clear");// clear the console to have better idea that we play
+        if(numberOfPlayer==1)
         {
-            cout<<"Sorry but you lose the word was :"<< word<< endl;
-            wordNotFound=true;
+            cout<<"You choose solo play so a word will be choose for you"<<endl;
+            word=findAWord();
+
         }
-    }
+        else
+        {
+            cout<<"You choose multiplayer play so choose a word"<< endl;
+            cout<<"Player one add a word"<< endl;
+            cin>> word;
+            std::system("clear");
+        }
+        int compteur(0);// variable to test how many time you faile
+        //1 Variable string to get word
+
+        bool wordFound(false);
+        bool wordNotFound(false);// variable who say if you win or not
+
+        cout<<"Word is shaked :"<<shaker(word) <<endl;
+        //2 shake letter
+
+        //3 ask until the word is found
+        while(!wordFound || !wordNotFound) // don't understand why double pipe don't react normally.
+        {
+            string playerTwoWord;
+            cout<<"enter a word"<< endl;
+
+            cin>>playerTwoWord;
+            if(playerTwoWord==word)
+            {
+             wordFound=true;
+             cout<<"Nice shot you win"<<endl;
+            }
+            compteur++;
+            if (compteur==4)
+            {
+                cout<<"Sorry but you lose the word was :"<< word<< endl;
+                wordNotFound=true;
+                break;
+            }
+        }
+        char playAgain;
+        cout<<"do you want to play again(y/n) ?"<<endl;
+        cin>>playAgain;
+        while(playAgain!='y'|| playAgain!='n')
+        {
+            if(playAgain=='n')
+            {
+                wantToPlayAgain=false;
+            }
+        }
+    }while(wantToPlayAgain);
+
  return 0;
 }
